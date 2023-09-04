c6aa532fadd9c939714597271e7ab59f83874550
Martin Keller <martin.keller@taketool.de>
2023-09-04 14:16:46 +0200

    description expanded

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Template and security info',
-    'description' => 'Provides a backend module for showing which templates included where as well as a couple of security and performance checks. This supports Typo3 maintenance, migration and security',
+    'title' => 'Template, extension and security info',
+    'description' => 'Provides a backend module for showing which templates included where, which extensions actually used as well as a couple of security and performance checks. This supports Typo3 maintenance, migration and security',
     'category' => 'module',
     'author' => 'Martin Keller',
     'author_email' => 'martin.keller@taketool.de',
