d829f39e2c2b51e7b192a397416d8e714a337419
Martin Keller <martin.keller@taketool.de>
2024-08-13 16:59:31 +0200

    upd sha1 to 11.5.39

--- Classes/Controller/Sha1Controller.php
+++ Classes/Controller/Sha1Controller.php
@@ -80,8 +80,8 @@ class Sha1Controller extends ActionController
     /**
      * compare all files in public/typo3 against precompiled SHA1 in Resources/Private/SHA1/ (~450kB each)
      * precompiled file generate:
-     * find ./typo3 -type f -name "*.php" -exec sha1sum {} \; | gzip > /Users/martin/github/typo3-ext-sysinfo/Resources/Private/SHA1/11.5.38/typo3_files_php.txt.gz
-     * find ./typo3 -type f -name "*.js" -exec sha1sum {} \; | gzip > /Users/martin/github/typo3-ext-sysinfo/Resources/Private/SHA1/11.5.38/typo3_files_js.txt.gz
+     * find ./typo3 -type f -name "*.php" -exec sha1sum {} \; | gzip > /Users/martin/github/typo3-ext-sysinfo/Resources/Private/SHA1/11.5.39/typo3_files_php.txt.gz
+     * find ./typo3 -type f -name "*.js" -exec sha1sum {} \; | gzip > /Users/martin/github/typo3-ext-sysinfo/Resources/Private/SHA1/11.5.39/typo3_files_js.txt.gz
 
      * a line looks like this: 5964dd3a9fcc9d3141415b1b8511b8938e1aabf0  ./typo3/index.php%
      *
